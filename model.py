0ed94df585f3024078bf6b7ad88880b6aec5d1f0
park <psw04181@naver.com>
2022-08-19 15:14:54 +0900

    Update model.py

--- model.py
+++ model.py
@@ -4,11 +4,12 @@ import torch.nn as nn
 class FCNetwork(nn.Module):
     def __init__(self, in_features):
         super(FCNetwork, self).__init__()
+        self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(in_features, 16)
         self.fc2 = nn.Linear(16, 1)
 
     def forward(self, x):
-        x = torch.flatten(x)
+        x = self.flatten(x)
         x = self.fc1(x)
         x = self.fc2(x)
 
